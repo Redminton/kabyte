ca9b38881ab5d2c4b28366a77d442e4997ee7e32
Redminton <133177595+Redminton@users.noreply.github.com>
2023-06-05 17:42:51 -0300

    tentei
    Co-Authored-By: KSB102030 <133717097+KSB102030@users.noreply.github.com>

--- cod.js
+++ cod.js
@@ -1,3 +1,8 @@
 function myfuncao() {
     document.getElementById("texto").innerHTML = "Paragraph changed.";
   }
+  function enter(){
+    let key = Event.key;
+   if (key=="enter" || key == "Enter"){
+    document.location='descricaogenerica.html' }
+  }

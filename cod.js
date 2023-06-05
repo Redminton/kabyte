19dcca9ec760d07af1343567241486a8cf0e71d4
Redminton <133177595+Redminton@users.noreply.github.com>
2023-06-05 19:13:48 -0300

    as
    Co-Authored-By: KSB102030 <133717097+KSB102030@users.noreply.github.com>

--- cod.js
+++ cod.js
@@ -1,8 +1,8 @@
 function myfuncao() {
     document.getElementById("texto").innerHTML = "Paragraph changed.";
   }
-  function enter(){
-    let key = Event.key;
+  function enter(event){
+    let key = event.key;
    if (key=="enter" || key == "Enter"){
     document.location='descricaogenerica.html' }
   }
